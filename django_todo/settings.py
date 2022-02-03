973bca47cbe85ab63712e43ab6a18ca9528c55d4
Olivertrain1221 <olivertrain1996@gmail.com>
2022-02-03 16:04:11 +0000

    Add final push before heroku setup

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://zuevivpqhwqjqd:30fc4eb11b5cad58ab8fe2db473f5f41f78d6047300c955e29478048afada47f@ec2-34-230-110-100.compute-1.amazonaws.com:5432/d9itasnujivojr')
 }
 
 
